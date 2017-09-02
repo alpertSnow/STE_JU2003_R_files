f0280da1828989b25bf3943754c04a94f983cfd4
alpertSnow <rmddxx@163.com>
2017-09-02 15:24:54 +0800

    02construct.R update to 3d version

--- 02construct.R
+++ 02construct.R
@@ -1,26 +1,33 @@
-### Construct source finder BUGS/JAGS model
-### using uniform prior, i.e. p(q) ~ U(qLower, qUpper)
-### using dcat() location prior, i.e. some location with 0 possibility
-### date: 2016-10-27
+############ Bayesian model maker #############
+### Construct source finder BUGS/JAGS model ###
+### using dcat() location prior,            ###
+### i.e. some location with 0 possibility   ###
+### This is a 3-d version                   ###
+### Date: 2017-09-02                        ###
+### By: xf                                  ###
+###############################################
 library(R2WinBUGS)
 library(coda)
 sf <- function(){
         # likelihood
-        ij <- (i-1) * nj + j    # grid location
+        ijk <- (k-1)*ni*nj + (j-1)*ni + i    # grid location
         for(m in 1 : M) {
-                a[m] <- H[m,ij]
+                a[m] <- H[m,ijk]
                 c[m] <- a[m] * q
                 mu[m] ~ dnorm(c[m],tau[m])
                 #mu[m] ~ dt(c[m],tau[m],2)
         }
         # prior
         # location prior
-        Mi ~ dcat(pi[])
+        Mk ~ dcat(pk[])
+        k <- kCat[Mk]
+        z <- zc[k]
+        Mj ~ dcat(pjk[1:nj,k])
+        j <- jCat[Mj]
+        y <- yc[j]
+        Mi <- dcat(pijk[1:ni,j,k])
         i <- iCat[Mi]
         x <- xc[i]
-        Mij ~ dcat(pij[1:nj,i])
-        j <- jCat[Mij]
-        y <- yc[j]
         #
         log.q ~ dunif(logqLower,logqUpper)
         q <- 10^log.q
