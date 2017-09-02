5e5dfe66504f280ed3098c4bf26f2c3615a4acaa
alpertSnow <rmddxx@163.com>
2017-09-02 16:25:39 +0800

    03runJags.R 3d version

--- 03runJags.R
+++ 03runJags.R
@@ -6,12 +6,9 @@ library(R2jags)
 library(coda)
 library(lattice)
 
-ni <- as.numeric(ni)
-nj <- as.numeric(nj)
-
-data <- list('nj', 'M', 'H', 'mu', 'tau','logqLower','logqUpper',
-             'xc', 'yc', 'pi', 'pij', 'iCat', 'jCat')
-parameters <- c('i','j','q','x','y')
+data <- list('ni', 'nj', 'M', 'H', 'mu', 'tau','logqLower','logqUpper',
+             'xc', 'yc', 'zc', 'pk', 'pjk', 'pijk', 'iCat', 'jCat', 'kCat')
+parameters <- c('i','j', 'k','q','x','y', 'z')
 inits <- NULL
 #inits <- function() {list(icon = 26, jcon = 31, q = 60)}
 
